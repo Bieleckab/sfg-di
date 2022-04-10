3e438b999bbb6ee112dca1f396d3fb9b83862804
Brano Bielecka <branislav.bielecka@gmail.com>
2022-04-10 22:56:30 +0200

    Added i18n profiles for EN, ES and example of DI of service dependent on profile selected

--- src/main/java/com/bielecka/di/DiApplication.java
+++ src/main/java/com/bielecka/di/DiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.bielecka.di.controllers.ConstructorInjectedController;
+import com.bielecka.di.controllers.I18nController;
 import com.bielecka.di.controllers.MyController;
 import com.bielecka.di.controllers.PropertyInjectedController;
 import com.bielecka.di.controllers.SetterInjectedController;
@@ -15,28 +16,23 @@ public class DiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
 
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
+		System.out.println("----------- Primary Bean");
 		MyController myController = (MyController) ctx.getBean("myController");
-
-		String greeting = myController.sayHello();
-
-		System.out.println(greeting);
+		System.out.println(myController.sayHello());
 
 		System.out.println("----------- Porperty");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
 		System.out.println(propertyInjectedController.getGreeting());
 
 		System.out.println("---------- Setter");
-
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
 		System.out.println(setterInjectedController.getGreeting());
 
 		System.out.println("---------- Constructor");
-
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
 		System.out.println(constructorInjectedController.getGreeting());
 
 	}
--- src/main/java/com/bielecka/di/controllers/I18nController.java
+++ src/main/java/com/bielecka/di/controllers/I18nController.java
@@ -1,15 +1,16 @@
 package com.bielecka.di.controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import com.bielecka.di.services.GreetingService;
 
 @Controller
-public class MyController {
+public class I18nController {
 
-	private final GreetingService greetingService;
+	GreetingService greetingService;
 
-	public MyController(GreetingService greetingService) {
+	public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
 
